748b72c12c7f255fc74e033555a0dbbfee26c9db
Peter Himschoot <peter@u2u.be>
2025-08-30 18:45:28 +0200

    Ready for lab

---
+++ src/U2U.Currencies.Infra/Migrations/20191007140218_seeding currencies.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace U2U.Currencies.Infra.Migrations
+{
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+  public partial class seedingcurrencies : Migration
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+  {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "USD");
+
+            migrationBuilder.UpdateData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "JPY");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "DLR");
+
+            migrationBuilder.UpdateData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "YEN");
+        }
+    }
+}
